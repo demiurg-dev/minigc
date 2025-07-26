1c684eb0f12c4168fa4b2d55017460ed747fa770
Ivan Radiček <iradicek@users.noreply.github.com>
2025-07-26 22:58:40 +0200

    Implment variables and let-expressions (#5)

---
+++ compiler/tests/variable.rs
@@ -0,0 +1,40 @@
+//! Let-expressions and variable usage.
+
+use minigc_compiler::codegen::CodegeGeneratorContext;
+use minigc_compiler::compile_expr;
+
+#[test]
+fn let_variable_usage() {
+    #[compile_expr]
+    mod test {
+        fn main(a: i64, b: i64) -> i64 {
+            let x: i64 = a + 3;
+            x + b
+        }
+    }
+    test.check().unwrap();
+    let ctx = CodegeGeneratorContext::default();
+    let main_mod = ctx.generate_module("main", test).init_runtime();
+    let main_fn = main_mod.get_fn2::<i64, i64, i64>("main");
+    assert_eq!(main_fn(31, 8), 42);
+}
+
+#[test]
+fn let_variable_scoping() {
+    #[compile_expr]
+    mod test {
+        fn main(a: i64, b: i64) -> i64 {
+            let x: i64 = a + 3;
+            let y: i64 = {
+                let x: i64 = b + 3;
+                x + 1
+            };
+            x + y
+        }
+    }
+    test.check().unwrap();
+    let ctx = CodegeGeneratorContext::default();
+    let main_mod = ctx.generate_module("main", test).init_runtime();
+    let main_fn = main_mod.get_fn2::<i64, i64, i64>("main");
+    assert_eq!(main_fn(17, 18), 42);
+}
