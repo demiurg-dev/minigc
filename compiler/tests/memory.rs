db31ae1dfe1c35f4e6baf08c677d10f92c6e38f1
Ivan R <ivan@radicek.com>
2025-08-02 15:12:51 +0200

    better tests

--- compiler/tests/memory.rs
+++ compiler/tests/memory.rs
@@ -8,7 +8,6 @@ fn empty_memory() {
     #[compile_expr]
     mod test {
         fn main() -> i64 {
-            let x: i64 = __minigc_alloc(7);
             __minigc_memory_size()
         }
     }
@@ -16,5 +15,22 @@ fn empty_memory() {
     let ctx = CodegeGeneratorContext::default();
     let main_mod = ctx.generate_module("main", test).init_runtime();
     let main_fn = main_mod.get_fn0::<i64>("main");
-    assert_eq!(main_fn(), 8);
+    assert_eq!(main_fn(), 0);
+}
+
+#[test]
+fn allocations() {
+    #[compile_expr]
+    mod test {
+        fn main() -> i64 {
+            let x: i64 = __minigc_alloc(7);
+            let x: i64 = __minigc_alloc(2);
+            __minigc_memory_size()
+        }
+    }
+    test.check().unwrap();
+    let ctx = CodegeGeneratorContext::default();
+    let main_mod = ctx.generate_module("main", test).init_runtime();
+    let main_fn = main_mod.get_fn0::<i64>("main");
+    assert_eq!(main_fn(), 16);
 }
